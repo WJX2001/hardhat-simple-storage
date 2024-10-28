d1013e07d07c8a1db99ea35a3fed29dab9c07fc3
WJX2001 <1758807220@qq.com>
2024-10-28 21:55:24 +0800

    feat: 程序化 验证合约

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox")
+require("@nomicfoundation/hardhat-verify");
 require("@chainlink/env-enc").config()
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SOPELIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 module.exports = {
   solidity: "0.8.27",
   defaultNetwork: "hardhat",
@@ -13,5 +15,10 @@ module.exports = {
       accounts: [`${PRIVATE_KEY}`],
       chainId: 11155111
     },
-  }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    }
+  },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,10 +9,42 @@ async function main () {
     'contract has been deployed successfully, contract address is' +
     simpleStorage.target,
   )
+
+  // what happens whne we deploy to our hardhat network
+  if(hre.network.config.network === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    // etherscan 可能还不知道这个交易 ，需要一段时间才能跟上区块链的变化
+    // 需要等待几个区块
+    console.log('waiting for 5 confirmations...')
+    await fundMe.deploymentTransaction().wait(5)
+  }
+
+  const currentValue = await simpleStorage.retrieve()
+  console.log(`current value is ${currentValue}`)
+
+  // Update the current value
+  const transactionResponse = await simpleStorage.store(7)
+  await transactionResponse.wait(1)
+  const updatedValue = await simpleStorage.retrieve()
+  console.log(`updated value is ${updatedValue}`)
+  
+
 }
 
-async function verify(contractAddress, args) {
-  
+async function verify (contractAddress, args) {
+  console.log("Verifying contract...")
+  try {
+    await hre.run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified")
+    } else {
+      console.log(e)
+    }
+  }
+
 }
 
 
