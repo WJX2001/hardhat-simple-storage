898b0c8996f220d7283a19d37951fe44086f058d
WJX2001 <1758807220@qq.com>
2024-10-28 20:31:17 +0800

    feat: first deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,20 @@
 
+const { ethers } = require("hardhat")
 
 async function main () {
-
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+  console.log("Deploying contract...")
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.waitForDeployment()
+  console.log(
+    'contract has been deployed successfully, contract address is' +
+    simpleStorage.target,
+  )
 }
 
-main().catch((error) => {
-  console.log(error)
-  process.exit(1)
-})
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
