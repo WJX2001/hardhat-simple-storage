0851563647ccdfbf96f4a4618bcc9f177e27b067
WJX2001 <1758807220@qq.com>
2024-10-29 23:26:31 +0800

    feat: hardha工具链

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,17 @@
 require("@chainlink/env-enc").config()
 require("@nomicfoundation/hardhat-toolbox")
-require("@nomicfoundation/hardhat-verify");
+require("@nomicfoundation/hardhat-verify")
 require("./tasks")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const SOPELIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const SOPELIA_RPC_URL = process.env.SEPOLIA_RPC_URL 
+const PRIVATE_KEY = process.env.PRIVATE_KEY 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 const LOCALHOST_URL = process.env.LOCALHOST_URL
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY
+// console.log(COINMARKET_API_KEY)
 module.exports = {
   solidity: "0.8.27",
   defaultNetwork: "hardhat",
@@ -27,4 +31,13 @@ module.exports = {
       sepolia: ETHERSCAN_API_KEY
     }
   },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketCap: COINMARKET_API_KEY,
+    token: "MATIC"
+  }
+
 }
