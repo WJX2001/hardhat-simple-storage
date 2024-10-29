b8bc87150e3e77e4ba011ec34c6a435d1b66ed33
WJX2001 <1758807220@qq.com>
2024-10-30 02:09:39 +0800

    feat: mock合约和正常合约部署流程 走通

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,23 +1,30 @@
 
-const { networkConfig } = require("../helper-hardhat-config")
 const { network } = require("hardhat")
-module.exports = async ({ getNameAccounts, deployments }) => {
-  const { deploy, log } = deployments
-  const { deoloyer } = await getNameAccounts()
-  const chainId = hre.network.config.chainId
-
-  const ethUsdPriceFeedAddress = networkConfig[network.config.chainId].ethUsdDataFeed
-
+const { devlopmentChains, networkConfig, LOCK_TIME, CONFIRMATIONS } = require("../helper-hardhat-config")
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy } = deployments
+  const { firstAccount } = await getNamedAccounts()
+  let dataFeedAddr
+  if (devlopmentChains.includes(network.name)) {
+    // 走mock流程
+    const mockV3Aggregator = await deployments.get("MockV3Aggregator")
+    dataFeedAddr = mockV3Aggregator.address
+  } else {
+    // 走正常流程 从help中获取测试网对应的喂价
+    dataFeedAddr = networkConfig[network.config.chainId].ethUsdDataFeed
+  }
 
 
 
   // well what happens when we want to change chains?
   // when going for localhost or hardhat network we want to use a mock
   const fundMe = await deploy("FundMe", {
-    from: deoloyer,
+    from: firstAccount,
     args: [
       // put price fedd address
+      dataFeedAddr
     ],
     log: true,
   })
 }
+module.exports.tags = ["all", "fundme"]
