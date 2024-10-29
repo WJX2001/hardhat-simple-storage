78ab8b311446db1e36cce1785142e54c25a7d9b1
WJX2001 <1758807220@qq.com>
2024-10-29 09:37:07 +0800

    feat: 配置文件修改

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,23 +1,24 @@
+require("@chainlink/env-enc").config()
 require("@nomicfoundation/hardhat-toolbox")
 require("@nomicfoundation/hardhat-verify");
-require("@chainlink/env-enc").config()
 require("./tasks")
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SOPELIA_RPC_URL = process.env.SEPOLIA_RPC_URL
-const PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY
+const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const LOCALHOST_URL = process.env.LOCALHOST_URL
 module.exports = {
   solidity: "0.8.27",
   defaultNetwork: "hardhat",
   networks: {
-    // sepolia: {
-    //   url: "https://sepolia.infura.io/v3/" + SOPELIA_RPC_URL,
-    //   accounts: [`${PRIVATE_KEY}`],
-    //   chainId: 11155111
-    // },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/" + SOPELIA_RPC_URL,
+      accounts: [`${PRIVATE_KEY}`],
+      chainId: 11155111
+    },
     localhost: {
-      url: "http://127.0.0.1:8545/",
+      url: LOCALHOST_URL,
       chainId: 31337
     }
   },
